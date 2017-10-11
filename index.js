2339d4ee9e331f6b6fd5f9e2e9e8c0e7971e1917
ND <singhnitesh028@gmail.com>
2017-10-11 16:43:52 +0530

    demo9

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry,Alert,AppState } from 'react-native';
 import App from './App';
-import CodePush from 'react-native-code push';
+import CodePush from 'react-native-code-push';
 import Push from 'mobile-center-push';
 // Push.setEventListener({
 //     pushNotificationRecieved:function({message='',title='<empty>'},{message += '\nCustom properties:\n'})
